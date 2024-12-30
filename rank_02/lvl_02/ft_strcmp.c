5f8ae316a90c7b18b1f7fead978af1ba4b40db09
Daniel Reule <dreule@3-B-12.42heilbronn.de>
2024-12-31 00:30:30 +0100

    added dirs

---
+++ rank_02/lvl_02/ft_strcmp.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int j = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+// int main(void)
+// {
+// 	char *str1 = "Hello!";
+// 	char *str2 = "Hello,!";
+
+// 	printf("OG: DIFF = %d\n", ft_strcmp(str1, str2));
+// 	printf("OG: DIFF = %d\n", strcmp(str1, str2));
+// }
---
+++ rank_02/lvl_04/flood_fill.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct	s_point
+{
+	int	x;
+	int	y;
+}	t_point;
+
+void	flood_fill(char **tab, t_point size, t_point begin)
+{
+	
+}
