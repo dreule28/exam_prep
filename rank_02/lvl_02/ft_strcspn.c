19656b4f242c3e160ba242a43429567561ff342b
Daniel Reule <dreule@2-I-7.42heilbronn.de>
2025-01-17 19:10:23 +0100

    improved ft_strcspn

---
+++ rank_02/lvl_02/ft_strcspn.c
@@ -0,0 +1,19 @@
+#include <string.h>
+
+size_t	ft_strcspn(const char *s, const char *reject)
+{
+	size_t i = 0;
+	size_t j = 0;
+
+	while (s[i])
+	{
+		while (reject[j])
+		{
+			if (s[i] == reject[j])
+				return (i);
+				j++;
+		}
+		i++;
+	}
+	return (i);
+}
