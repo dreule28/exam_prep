81fd18c1562bcfb329f76db8baec88059f57beef
Daniel Reule <dreule@2-I-7.42heilbronn.de>
2025-01-30 11:24:00 +0100

    fix ft_rrange

--- rank_02/lvl_03/ft_rrange.c
+++ rank_02/lvl_03/ft_rrange.c
@@ -1,16 +1,21 @@
 #include <stdlib.h>
 
+int	ft_abs(nb)
+{
+	if (nb < 0)
+		return (-nb);
+	return (nb);
+}
+
 int	*ft_rrange(int start, int end)
 {
 	int *range;
 	int i = 0;
 	int step = 1;
-	int n = end - start;
+	int n = ft_abs(end - start) + 1;
 
 	if (end > start)
 		step = -1;
-	n++;
-
 	range = malloc(sizeof(int) * n);
 	if (!range)
 		return (NULL);
