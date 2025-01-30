3e47cef5f5ea3f8b9a1f8316608148cfccac4865
Daniel Reule <dreule@2-I-7.42heilbronn.de>
2025-01-30 11:31:11 +0100

    fix alpha_mirror

--- rank_02/lvl_02/alpha_mirror.c
+++ rank_02/lvl_02/alpha_mirror.c
@@ -1,21 +1,18 @@
 #include <unistd.h>
-
-int	main(int argc, char **argv)
+int	main(int argc, char *argv[])
 {
-	if (argc != 2)
-	{
-		write(1, "\n", 1);
-		return (1);
-	}
 	int i = 0;
-	while (argv[1][i])
+	if (argc == 2)
 	{
-		if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
-			argv[1][i] = 'z' - (argv[1][i] - 'a');
-		else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-			argv[1][i] = 'Z' - (argv[1][i] - 'A');
-		write(1, &argv[1][i], 1);
-		i++;
+		while (argv[1][i] != '\0')
+		{
+			if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+				argv[1][i] = ('a' + 'z') - argv[1][i];
+			else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+				argv[1][i] = ('A' + 'Z') - argv[1][i];
+			write(1, &argv[1][i],1);
+			i++;
+		}
 	}
 	write(1, "\n", 1);
 	return (0);
