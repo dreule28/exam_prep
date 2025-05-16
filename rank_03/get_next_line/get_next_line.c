d51923db983d7bd68dfe5a2dc40095e9ec63e7f1
dreule <danielreule28@gmail.com>
2025-05-16 09:36:46 +0200

    GNL FIX

--- rank_03/get_next_line/get_next_line.c
+++ rank_03/get_next_line/get_next_line.c
@@ -2,30 +2,59 @@
 
 char	*ft_strdup(char *str)
 {
-	char *dup;
-	int i = 0;
+	char	*duplicate;
+	int		i = 0;
 
 	while (str[i])
 		i++;
-	dup = malloc(i + 1);
-	if (!dup)
+	duplicate = malloc(i + 1);
+	if (!duplicate)
 		return (NULL);
 	i = 0;
 	while (str[i])
 	{
-		dup[i] = str[i];
+		duplicate[i] = str[i];
 		i++;
 	}
-	dup[i] == '\0';
-	return (dup);
+	duplicate[i] = '\0';
+	return (duplicate);
 }
 
 char	*get_next_line(int fd)
 {
-	static int bytes_read = 0;
-	static int bytes_given = 0;
-	static char buffer[BUFFER_SIZE];
-	char	line[1000003];
+	static int	bytes_read = 0;
+	static int	bytes_given = 0;
+	static char	buffer[BUFFER_SIZE];
+	char		line[1000003];
+	int 		i = 0;
 
-	if (fc <)
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	while (1)
+	{
+		if (bytes_given >= bytes_read)
+		{
+			bytes_read = read(fd, buffer, BUFFER_SIZE);
+			bytes_given = 0;
+			if (bytes_read <= 0)
+				break;
+		}
+		line[i++] = buffer[bytes_given++];
+		line[i] = '\0';
+		if (buffer[bytes_read - 1] == '\n')
+			break;
+	}
+	if (i == 0)
+		return(NULL);
+	return (ft_strdup(line));
+}
+
+
+int	main(void)
+{
+	int fd = open("get_next_line.h", O_RDONLY);
+	char *line = get_next_line(fd);
+	printf("%s\n", line);
+	free(line);
+	return (0);
 }
--- rank_03/get_next_line/get_next_line.h
+++ rank_03/get_next_line/get_next_line.h
@@ -1,10 +1,10 @@
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
 	#ifndef BUFFER_SIZE
-	#define BUFFER_SIZE 42
+	#define BUFFER_SIZE 25
 	#endif
-#include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
 #endif
